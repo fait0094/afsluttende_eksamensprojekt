2f9dc4a547dfb563fe1d0fd567233b273dc233ab
Faith Mansilungan <faith0094@stud.kea.dk>
2023-12-11 13:41:00 +0100

    XML Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ... other configurations
+
+  site: 'https://emfs-eksamen.netlify.app/',
+  integrations: [sitemap()],
+});

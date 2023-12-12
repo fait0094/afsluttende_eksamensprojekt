94ef397472eac022b9053396bec8b586c58775a3
Sofie Hemmingsen <sofi413c@stud.kea.dk>
2023-12-12 08:53:58 +0100

    astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   // ... other configurations
 
   site: 'https://emfs-eksamen.netlify.app/',
-  integrations: [sitemap()],
+  integrations: [sitemap()]
 });
